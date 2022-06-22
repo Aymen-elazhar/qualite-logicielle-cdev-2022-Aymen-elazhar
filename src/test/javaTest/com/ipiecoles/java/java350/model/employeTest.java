63cfd064e117a1240bafc8a1b16367a37c7aecf3
admin <Juliettemimenne42>
2022-06-22 10:06:22 +0200

    testOk

--- src/test/javaTest/com/ipiecoles/java/java350/model/employeTest.java
+++ src/test/javaTest/com/ipiecoles/java/java350/model/employeTest.java
@@ -2,6 +2,9 @@ package com.ipiecoles.java.java350.model;
 
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import java.time.LocalDate;
 
 public class employeTest {
@@ -37,5 +40,26 @@ public class employeTest {
 
         Assertions.assertThat(2).isEqualTo(employe.getNombreAnneeAnciennete());
 
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'M12345',0,1,1.0,1700.0",
+            "'M12345',0,0,1.0,1700.0",
+            "'T12345',0,1,1.0,1000.0",
+            "'T12345',0,0,1.0,300.0",
+    })
+    public void testGetPrimeAnnuelle(String matricule,
+                                     Integer nbAnneesAnciennete,
+                                     Integer performance,
+                                     Double tauxTravail,
+                                     Double primeCalculee){
+        //Given
+        Employe employe = new Employe("Doe", "John", matricule, LocalDate.now().minusYears(nbAnneesAnciennete),
+                2500d, performance, tauxTravail);
+        //When
+        Double prime = employe.getPrimeAnnuelle();
+        //Then
+        Assertions.assertThat(prime).isEqualTo(primeCalculee);
     }
 }
